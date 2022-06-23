91c4e676863c16e2a39117e56fcf28e7fb1240ae
juwon8891 <juwon8891@gmail.com>
2022-06-23 11:46:47 +0900

    [docs]README 파일 커밋 컨벤션 룰 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
-    public MemberServiceImpl(MemoryMemberRepository memoryMemberRepository) {
+    public MemberServiceImpl(MemberRepository memoryMemberRepository) {
         this.memberRepository = memoryMemberRepository;
     }
 
